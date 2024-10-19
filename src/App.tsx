330ee9f9fab788e59a4a902c3b290e82b44ed38f
Marcus Martins <marcusmartins38@gmail.com>
2024-10-19 19:00:50 -0400

    feat: create table component

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import { useState } from 'react';
 import './App.css';
 import Sidebar from './components/Sidebar';
+import Tag from './components/Tag';
+import TransactionTable from './components/TransactionTable';
 
 function App() {
   const [count, setCount] = useState(0);
@@ -27,9 +29,7 @@ function App() {
                   <span>Total Income</span>
                   <h2 className="font-bold text-2xl">$632.000</h2>
                 </div>
-                <div>
-                  <span>+1.29%</span>
-                </div>
+                <Tag text="+5.10%" color="green" />
               </div>
               <div className="flex flex-row items-center justify-between w-full max-w-80 py-8 px-6 bg-base-200 rounded-2xl mb-2 mr-2">
                 <div className="w-[45px] h-[45px] flex items-center justify-center rounded-lg bg-primary">
@@ -39,11 +39,11 @@ function App() {
                   <span>Total Income</span>
                   <h2 className="font-bold text-2xl">$632.000</h2>
                 </div>
-                <div>
-                  <span>+1.29%</span>
-                </div>
+                <Tag text="-1.30%" color="red" />
               </div>
             </section>
+
+            <TransactionTable />
           </div>
 
           <div>Qualquer coisa</div>
---
+++ src/components/TransactionTable.tsx
@@ -0,0 +1,96 @@
+import React from 'react';
+
+export default function TransactionTable() {
+  return (
+    <div className="overflow-x-auto bg-base-200 rounded-2xl">
+      <table className="table">
+        {/* head */}
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Date</th>
+            <th>Amount</th>
+            <th>Status</th>
+          </tr>
+        </thead>
+        <tbody>
+          {/* row 1 */}
+          <tr>
+            {/* <th>
+              <label>
+                <input type="checkbox" className="checkbox" />
+              </label>
+            </th> */}
+            <td>
+              <div className="flex items-center gap-3">
+                <div className="avatar">
+                  <div className="mask mask-squircle h-12 w-12">
+                    <img
+                      src="https://img.daisyui.com/images/profile/demo/2@94.webp"
+                      alt="Avatar Tailwind CSS Component"
+                    />
+                  </div>
+                </div>
+                <div>
+                  <div className="font-bold">Hart Hagerty</div>
+                </div>
+              </div>
+            </td>
+            <td>Zemlak, Daniel and Leannon</td>
+            <td>Purple</td>
+            <td>
+              <span>Deposited</span>
+            </td>
+          </tr>
+          {/* row 2 */}
+          <tr>
+            <td>
+              <div className="flex items-center gap-3">
+                <div className="avatar">
+                  <div className="mask mask-squircle h-12 w-12">
+                    <img
+                      src="https://img.daisyui.com/images/profile/demo/3@94.webp"
+                      alt="Avatar Tailwind CSS Component"
+                    />
+                  </div>
+                </div>
+                <div>
+                  <div className="font-bold">Brice Swyre</div>
+                </div>
+              </div>
+            </td>
+            <td>Carroll Group</td>
+            <td>Red</td>
+            <td>
+              <span>Deposited</span>
+            </td>
+          </tr>
+          {/* row 3 */}
+          <tr>
+            <td>
+              <div className="flex items-center gap-3">
+                <div className="avatar">
+                  <div className="mask mask-squircle h-12 w-12">
+                    <img
+                      src="https://img.daisyui.com/images/profile/demo/4@94.webp"
+                      alt="Avatar Tailwind CSS Component"
+                    />
+                  </div>
+                </div>
+                <div>
+                  <div className="font-bold">Marjy Ferencz</div>
+                </div>
+              </div>
+            </td>
+            <td>Rowe-Schoen</td>
+            <td>Crimson</td>
+            <td>
+              <span>Deposited</span>
+            </td>
+          </tr>
+        </tbody>
+        {/* foot */}
+      </table>
+    </div>
+  );
+}
