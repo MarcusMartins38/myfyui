b1ab1a8bb10f196906b30ce0a2fc38fcb2459049
Marcus Martins <marcusmartins38@gmail.com>
2024-10-19 16:10:59 -0400

    feat: color mytheme daisyui

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,32 @@ export default {
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [require('daisyui'),],
+  daisyui: {
+    themes: [{
+        mytheme: {
+            "primary": "#6359E9",
+            "primary-content": "#dbd9ff",
+            "secondary": "#65cff6",
+            "secondary-content": "#000608",
+            "accent": "#00bf00",
+            "accent-content": "#000d00",
+            "neutral": "#18080c",
+            "neutral-content": "#ccc6c7",
+            "base-100": "#141332",
+            "base-200": "#1d1d41",
+            "base-300": "#1a1d25",
+            "base-content": "#cfd0d3",
+            "info": "#00bbee",
+            "info-content": "#000d14",
+            "success": "#02b15a",
+            "success-content": "#061200",
+            "warning": "#f87f00",
+            "warning-content": "#150600",
+            "error": "#E41414",
+            "error-content": "#ffd8d7",
+        },
+    }],
+    },
 }
 
