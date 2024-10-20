69081a5556b2f1ef9d822162f137c70f456b44c3
Marcus Martins <marcusmartins38@gmail.com>
2024-10-20 17:09:04 -0400

    feat: json-server

--- src/types.ts
+++ src/types.ts
@@ -1,7 +1,9 @@
 export type TrasactionStatusT = 'income' | 'outcome';
 export type TransactionT = {
+  id: number;
   name: string;
   date: Date;
   amount: number;
   status: TrasactionStatusT;
+  userId: number;
 };
