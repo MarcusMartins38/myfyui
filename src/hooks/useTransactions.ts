3791f568bc6fb2aab0d790f7f34f5c20eae946c0
Marcus Martins <marcusmartins38@gmail.com>
2024-10-20 18:16:21 -0400

    refactor: change some things from custom hook

--- src/hooks/useTransactions.ts
+++ src/hooks/useTransactions.ts
@@ -5,26 +5,25 @@ import { addManyTransactions } from '../redux/transaction/transactionActions';
 
 export const useTransactions = () => {
   const dispatch = useDispatch();
+
   const fetchTransactions = async () => {
     const response = await fetch('http://localhost:5000/transactions');
     if (!response.ok) {
       throw new Error('Network response was not ok');
     }
-
     return response.json();
   };
 
-  const { data, isLoading } = useQuery({
+  const { data, isLoading, error } = useQuery({
     queryKey: ['transactions'],
     queryFn: fetchTransactions,
   });
 
   useEffect(() => {
-    console.log({ data, isLoading });
     if (!isLoading) {
       dispatch(addManyTransactions(data));
     }
   }, [isLoading, dispatch]);
 
-  return { isLoading };
+  return { data, isLoading, error };
 };
