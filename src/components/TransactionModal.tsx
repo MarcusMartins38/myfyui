859c7f38bac62a85ba305e4d2e35c772cf0bc69b
Marcus Martins <marcusmartins38@gmail.com>
2024-10-20 14:36:28 -0400

    feat: open transaction modal

---
+++ src/components/TransactionModal.tsx
@@ -0,0 +1,92 @@
+import React, { useState } from 'react';
+
+type TransactionModal = {
+  isOpen: boolean;
+  setIsModalOpen: (isOpen: boolean) => void;
+};
+
+const TransactionModal = ({ isOpen, setIsModalOpen }: TransactionModal) => {
+  const [transactionType, setTransactionType] = useState('income');
+  const [description, setDescription] = useState('');
+  const [amount, setAmount] = useState('');
+
+  const handleClose = () => setIsModalOpen(false);
+  const handleSave = () => {
+    handleClose();
+  };
+
+  return (
+    <>
+      {isOpen && (
+        <div className="modal modal-open">
+          <div className="modal-box">
+            <h2 className="font-bold text-lg">Add Transaction</h2>
+
+            <div className="form-control mb-4">
+              <label className="label">
+                <span className="label-text">Name</span>
+              </label>
+              <input
+                type="text"
+                className="input input-bordered"
+                value={description}
+                onChange={(e) => setDescription(e.target.value)}
+              />
+            </div>
+
+            <div className="form-control mb-4">
+              <label className="label">
+                <span className="label-text">Amount</span>
+              </label>
+              <input
+                type="text"
+                className="input input-bordered"
+                value={amount}
+                onChange={(e) => setAmount(e.target.value)}
+              />
+            </div>
+
+            <div className="flex flex-col items-start mb-4">
+              <span className="label-text mb-1">Transaction Type</span>
+              <div className="flex items-center mt-2">
+                <label className="mr-4">
+                  <input
+                    type="radio"
+                    name="transactionType"
+                    value="income"
+                    checked={transactionType === 'income'}
+                    onChange={() => setTransactionType('income')}
+                    className="radio"
+                  />
+                  <span className="ml-2">Income</span>
+                </label>
+                <label>
+                  <input
+                    type="radio"
+                    name="transactionType"
+                    value="outcome"
+                    checked={transactionType === 'outcome'}
+                    onChange={() => setTransactionType('outcome')}
+                    className="radio"
+                  />
+                  <span className="ml-2">Outcome</span>
+                </label>
+              </div>
+            </div>
+
+            <div className="modal-action">
+              <button className="btn" onClick={handleClose}>
+                Close
+              </button>
+              <button className="btn btn-primary" onClick={handleSave}>
+                Save
+              </button>
+            </div>
+          </div>
+        </div>
+      )}
+    </>
+  );
+};
+
+export default TransactionModal;
--- src/components/TransactionTable.tsx
+++ src/components/TransactionTable.tsx
@@ -1,6 +1,7 @@
-import React, { useState } from 'react';
+import { useState } from 'react';
 import { formatAmout, formatDate } from '../lib/utils';
 import Tag from './Tag';
+import TransactionModal from './TransactionModal';
 
 type TransactionT = {
   name: string;
@@ -15,6 +16,7 @@ const colorTailwindMap = {
 };
 
 export default function TransactionTable() {
+  const [isModalOpen, setIsModalOpen] = useState(false);
   const [transactionState, setTransactionState] = useState<TransactionT[]>([
     {
       name: 'Market',
@@ -24,49 +26,57 @@ export default function TransactionTable() {
     },
   ]);
 
+  const handleOpen = () => setIsModalOpen(true);
+
   const handleAddTransactionClick = () => {};
   return (
-    <div className="overflow-x-auto bg-base-200 rounded-2xl">
-      <header className="flex flex-row w-[calc(100%-8px)] m-1">
-        <button className="btn hover:bg-success/50">Add Transaction</button>
-      </header>
+    <>
+      <TransactionModal isOpen={isModalOpen} setIsModalOpen={setIsModalOpen} />
 
-      <table className="table">
-        <thead>
-          <tr>
-            <th>Name</th>
-            <th>Date</th>
-            <th>Amount</th>
-            <th>Status</th>
-          </tr>
-        </thead>
-        <tbody>
-          {transactionState.map((transaction) => (
+      <div className="overflow-x-auto bg-base-200 rounded-2xl">
+        <header className="flex flex-row w-[calc(100%-8px)] m-1">
+          <button className="btn hover:bg-success/50" onClick={handleOpen}>
+            Add Transaction
+          </button>
+        </header>
+
+        <table className="table">
+          <thead>
             <tr>
-              <td>
-                <div className="flex items-center gap-3">
-                  <div className="avatar">
-                    <div className="mask mask-squircle h-10 w-10">
-                      <img
-                        src="https://img.daisyui.com/images/profile/demo/2@94.webp"
-                        alt="Avatar Tailwind CSS Component"
-                      />
+              <th>Name</th>
+              <th>Date</th>
+              <th>Amount</th>
+              <th>Status</th>
+            </tr>
+          </thead>
+          <tbody>
+            {transactionState.map((transaction) => (
+              <tr>
+                <td>
+                  <div className="flex items-center gap-3">
+                    <div className="avatar">
+                      <div className="mask mask-squircle h-10 w-10">
+                        <img
+                          src="https://img.daisyui.com/images/profile/demo/2@94.webp"
+                          alt="Avatar Tailwind CSS Component"
+                        />
+                      </div>
+                    </div>
+                    <div>
+                      <div className="font-bold">{transaction.name}</div>
                     </div>
                   </div>
-                  <div>
-                    <div className="font-bold">{transaction.name}</div>
-                  </div>
-                </div>
-              </td>
-              <td>{formatDate(transaction.date)}</td>
-              <td>{formatAmout(transaction.amount)}</td>
-              <td>
-                <Tag text={transaction.status} color={colorTailwindMap[transaction.status]} />
-              </td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
-    </div>
+                </td>
+                <td>{formatDate(transaction.date)}</td>
+                <td>{formatAmout(transaction.amount)}</td>
+                <td>
+                  <Tag text={transaction.status} color={colorTailwindMap[transaction.status]} />
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </>
   );
 }
